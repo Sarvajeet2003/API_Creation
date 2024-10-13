f8447bed452622125a6fbc05f64ce5c6f62f8219
Nabh Rajput <108608004+nabhrajput@users.noreply.github.com>
2024-10-13 15:15:12 +0530

    Added Repository for last 7 main API

--- src/main/java/com/example/mongodbapi/repository/MoleculeRepository.java
+++ src/main/java/com/example/mongodbapi/repository/MoleculeRepository.java
@@ -2,6 +2,7 @@ package com.example.mongodbapi.repository;
 
 import com.example.mongodbapi.model.Molecule;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,5 +17,18 @@ public interface MoleculeRepository extends MongoRepository<Molecule, String> {
     List<Molecule> findByFunctionalGroups(String functional_groups);  // Correct field name
     List<Molecule> findByFlavorProfile(String flavorProfile);
     List<Molecule> findByFemaFlavorProfile(String femaFlavorProfile);
+
+    @Query("{ 'molecular_weight' : { $gte: ?0 } }")
+    List<Molecule> findByMolecularWeightGreaterThanEqual(double molecularWeight);
+
+
+    @Query("{ 'molecular_weight' : { $gte: ?0, $lte: ?1 } }")
+    List<Molecule> findByExactMassBetween(double from, double to);
+
+    @Query("{ 'hbd_count' : ?0 }")
+    List<Molecule> findByHbdCount(int hbd_count);
+
+    @Query("{ 'hba_count' : ?0 }")
+    List<Molecule> findByHbaCount(int hba_count);
 
 }
